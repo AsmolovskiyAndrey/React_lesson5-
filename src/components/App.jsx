52b445d3489aad72afabed99de1bd65596ad0bc7
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2022-12-26 13:37:53 +0100

    breed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,21 +5,35 @@ import Select from 'react-select';
 axios.defaults.baseURL = 'https://api.thedogapi.com/v1';
 axios.defaults.headers.common['x-api-key'] = process.env.REACT_APP_API_KEY;
 
-// const options = [
-//   { value: 'chocolate', label: 'Chocolate' },
-//   { value: 'strawberry', label: 'Strawberry' },
-//   { value: 'vanilla', label: 'Vanilla' },
-// ];
-
 export class App extends Component {
+  state = {
+    breeds: [],
+  };
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get('/breeds');
+      this.setState({ breeds: response.data });
+    } catch (error) {}
+  }
+
   selectBreed = option => {
     console.log(option);
   };
 
+  buildSelectOptions = () => {
+    return this.state.breeds.map(breed => ({
+      value: breed.id,
+      label: breed.name,
+    }));
+  };
+
   render() {
+    const options = this.buildSelectOptions();
+
     return (
       <>
-        <Select options={[]} onChange={this.selectBreed} />
+        <Select options={options} onChange={this.selectBreed} />
       </>
     );
   }
