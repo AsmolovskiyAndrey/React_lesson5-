e98c0951eaa37c60cafe442bcbf3e05fcb3e516b
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2022-12-26 13:06:37 +0100

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,26 @@
-export const App = () => {
-  return <>React homework template</>;
-};
+import axios from 'axios';
+import { Component } from 'react';
+import Select from 'react-select';
+
+axios.defaults.baseURL = 'https://api.thedogapi.com/v1';
+axios.defaults.headers.common['x-api-key'] = process.env.REACT_APP_API_KEY;
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' },
+];
+
+export class App extends Component {
+  selectBreed = option => {
+    console.log(option);
+  };
+
+  render() {
+    return (
+      <>
+        <Select options={[]} onChange={this.selectBreed} />
+      </>
+    );
+  }
+}
